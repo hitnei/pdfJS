6dcf67f77e8b7a7b6cd3bb49246885ce785d9db4
hitnei <hoangnhi050298@gmail.com>
2020-08-04 14:45:24 +0700

    push bug

--- web/me.js
+++ web/me.js
@@ -3,35 +3,50 @@ window.onload = function(e) {
     const viewers = document.getElementById('viewer');
     var viewerList
 
-    console.log("kho vcc");
+    // add block to #viewer
+    // var newBlock = document.createElement("span");
+    // newBlock.createTextNode = "some node"
+    // console.log(newBlock);
 
     var dragItem = null;
 
     blocks.forEach(block => {
         block.addEventListener('dragstart', function(e) {
             dragItem = block
-            viewerList = viewers.querySelectorAll('.textLayer > span')
+            viewerList = viewers.querySelectorAll('.textLayer > *')
             setTimeout(() => {
                 block.style.display = 'none'
             }, 0)
-            viewerList.forEach((viewer) => {
-                viewer.addEventListener('dragover', function(e) {
-                    e.preventDefault()
-                })
-                viewer.addEventListener('dragenter', function(e) {
-                    e.preventDefault()
-                })
-                viewer.addEventListener('drop', function(e) {
-                    this.append(dragItem)
-                })
-            })
         })
         block.addEventListener('dragend', function(e) {
-            console.log('e ' + e);
+            dragItem.style.position = 'absolute';
+            dragItem.style.left = e.clientX + 'px';
+            dragItem.style.top = e.clientY + 'px';
+
+            // dragItem.style.left = Math.abs(e.screenX + 1000) + 'px';
+            // dragItem.style.top = e.screenY + 'px';
+
             setTimeout(() => {
                 dragItem.style.display = 'inline-block'
                 dragItem = null
             }, 0)
+        })
+    })
+
+    console.log(viewerList);
+    viewerList.forEach((viewer) => {
+        viewer.addEventListener('dragstart', function(e) {
+            console.log("dragstart");
+            // e.preventDefault()
+        })
+        viewer.addEventListener('dragenter', function(e) {
+            console.log("dragenter");
+            // e.preventDefault()
+        })
+        viewer.addEventListener('drop', function(e) {
+            console.log("drop");
+            console.log(block);
+            this.append(dragItem)
         })
     })
 }
