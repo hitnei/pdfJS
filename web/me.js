91a6c15a30e1bdd16a708bd8fb18ddf770bf84b7
hitnei <hoangnhi050298@gmail.com>
2020-08-04 10:52:08 +0700

    before toang

--- web/me.js
+++ web/me.js
@@ -1,40 +1,35 @@
-window.onload = function (e) {
+window.onload = function(e) {
     const blocks = document.querySelectorAll('.some-block');
     const viewers = document.getElementById('viewer');
     var viewerList
-    
-    console.log("kho vcc");
 
-    // console.log(viewerList.length);
+    console.log("kho vcc");
 
     var dragItem = null;
 
-    console.log(viewers);
     blocks.forEach(block => {
-        console.log(block);
-        block.addEventListener('dragstart', function (e) {
+        block.addEventListener('dragstart', function(e) {
             dragItem = block
             viewerList = viewers.querySelectorAll('.textLayer > span')
-            console.log(viewerList);
             setTimeout(() => {
                 block.style.display = 'none'
             }, 0)
             viewerList.forEach((viewer) => {
-                viewer.addEventListener('dragover', function (e) {
+                viewer.addEventListener('dragover', function(e) {
                     e.preventDefault()
                 })
-                viewer.addEventListener('dragenter', function (e) {
+                viewer.addEventListener('dragenter', function(e) {
                     e.preventDefault()
                 })
-                viewer.addEventListener('drop', function (e) {
-                    console.log('drop');
+                viewer.addEventListener('drop', function(e) {
                     this.append(dragItem)
                 })
             })
         })
-        block.addEventListener('dragend', function (e) {
+        block.addEventListener('dragend', function(e) {
+            console.log('e ' + e);
             setTimeout(() => {
-                dragItem.style.display = 'block'
+                dragItem.style.display = 'inline-block'
                 dragItem = null
             }, 0)
         })
